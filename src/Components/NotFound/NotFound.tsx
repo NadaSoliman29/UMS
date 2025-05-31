08b8d7f2849d8bd1378243f90fd0c3543d61b9b0
Nadasoliman29 <nadaezzat87147@gmail.com>
2025-05-31 13:33:00 +0300

    change message

--- src/Components/NotFound/NotFound.tsx
+++ src/Components/NotFound/NotFound.tsx
@@ -1,13 +1,16 @@
+import notFoundImg from "../../assets/images/404-not-found.png"
 
-import notfound from "../../assets/Images/404-not-found.png"
 export default function NotFound() {
   return (
-   <>
-    <div className='d-flex justify-content-center'>
- <img src={notfound}  className='w-50' />
-
-   </div>
-   
-   </>
-  )
+    <div className="text-center p-5">
+      <img
+        src={notFoundImg}
+        alt="404 Not Found"
+        className="img-fluid"
+        style={{ maxHeight: '400px' }}
+      />
+      <h2 className="mt-4 text-danger">404 - Page Not Found</h2>
+      <p className="text-muted">Sorry, the page you’re looking for doesn’t exist.</p>
+    </div>
+  );
 }
